d844df7e4cc697fb240cc647ada0633bda688036
West Kraemer <WestKraemer@gmail.com>
2017-01-19 14:48:01 -0500

    add layout and code to store data entered into text field

--- phoneNumber/ViewController.swift
+++ phoneNumber/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet var numberField: UITextField!
+    
+    
+    @IBAction func Save(_ sender: Any) {
+        
+        UserDefaults.standard.set(numberField.text, forKey: "number")
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let numberObject = UserDefaults.standard.object(forKey: "number")
+        
+        if let number = numberObject as? String {
+            
+            numberField.text = number
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
